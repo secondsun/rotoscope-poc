6ada838132d36c783cc137d2cbd01778fead657d
Summers Pittman <secondsun@gmail.com>
2025-04-05 15:02:05 -0400

    Adding source of jSystemThemeDetector to ignore jpms issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,25 @@ dependencies {
     api(libs.datastore.preferences)
     api(libs.datastore)
     implementation(libs.jetbrains.lifecycle)
-    implementation(libs.jSystemThemeDetector)
+    //implementation(libs.jSystemThemeDetector)
     implementation(compose.materialIconsExtended)
+
+
+    implementation ("org.slf4j:slf4j-api:1.7.32")
+    //JNA
+    implementation ("net.java.dev.jna:jna-jpms:5.17.0")
+    implementation ("net.java.dev.jna:jna-platform-jpms:5.17.0")
+
+    //JFA
+    implementation ("de.jangassen:jfa:1.2.0") {exclude(group = "net.java.dev.jna", module = "jna")}
+
+    //OSHI
+    implementation ("com.github.oshi:oshi-core:5.8.6")
+
+    implementation ("io.github.g00fy2:versioncompare:1.4.1")
+
+    implementation ("org.jetbrains:annotations:22.0.0")
+
 }
 
 
